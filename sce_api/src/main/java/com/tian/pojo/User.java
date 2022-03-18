b4843e4708b4df95b7a246407a974205b973458c
tian <1120018808@qq.com>
2022-03-18 21:51:06 +0800

    1.0.1

--- sce_api/src/main/java/com/tian/pojo/User.java
+++ sce_api/src/main/java/com/tian/pojo/User.java
@@ -2,14 +2,13 @@ package com.tian.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class User implements Serializable {
+
     private Integer id;
     private String name;
     private String sex;
